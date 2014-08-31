bebd6cf6a8845f1a26ee4ce3076e3a8632460e32
Shiro Kawai <shiro@acm.org>
2014-08-31 05:31:18 -1000

    Add IP blacklist for posting

--- tests/poster.scm
+++ tests/poster.scm
@@ -20,7 +20,8 @@
    "../chaton-poster"
    :environment `(("GAUCHE_LOAD_PATH" . "..")
                   ("CHATON_DATADIR" . ,*testdatadir*)
-                  ("CHATON_DOCDIR"  . ,*testdatadir*))
+                  ("CHATON_DOCDIR"  . ,*testdatadir*)
+                  ("CHATON_LOGDIR" . ,*testdatadir*))
    :parameters `((nick . ,nick) (text . ,text))))
 
 (define (check nick text)
@@ -40,6 +41,34 @@
 (test-post "Nick.name" "text2")
 (test-post "Nock.name" "text3")
 (test-post "Nock.name" "<x>oo</x>http://text4/foo?bar#baz<s>yy</s>")
+
+;; blacklist test
+(with-output-to-file (build-path *testdatadir* "blacklist.txt")
+  (^[]
+    (print "# test blacklist entry")
+    (print "127.0.0.1")))
+
+(define (test-post-blacklisted nick text)
+  (test* (format "post/blacklisted ~a ~a" nick text) #t
+         (begin (post nick text)
+                (not (check nick text)))))
+
+(test-post-blacklisted "Nuke.name" "text4")
+
+(with-output-to-file (build-path *testdatadir* "blacklist.txt")
+  (^[]
+    (print "# test blacklist entry")
+    (print "10.0.0.1")))
+
+(test-post "Nuke.name" "text5")
+
+(with-output-to-file (build-path *testdatadir* "blacklist.txt")
+  (^[]
+    (print "# test blacklist entry")
+    (print "127.0.0.0/8")))
+
+(test-post-blacklisted "Nuke.name" "text6")
+
 
 (test-end)
 
