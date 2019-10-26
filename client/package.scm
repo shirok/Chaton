5a4749396998400fc521f645f51f5b2495ff3609
Shiro Kawai <shiro@acm.org>
2019-10-26 10:08:01 -1000

    Modernize configure in client/

---
+++ client/package.scm
@@ -0,0 +1,17 @@
+;;
+;; Package chaton-client
+;;
+
+(define-gauche-package "Chaton-client"
+  :version "1.0"
+  :description "Client library of Chaton chat server"
+  :require (("Gauche" (>= "0.9.8")))
+  :providing-modules (chaton.client)
+  :authors ("Shiro Kawai <shiro@acm.org>")
+  :maintainers ()
+  :licenses ("BSD")
+  :homepage "http://chaton.practical-scheme.net/"
+  :repository "https://github.com/shork/Chaton"
+  )
+
+
