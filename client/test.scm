a78e0ba873818eea06a4233ec19772747ed3309e
Shiro Kawai <shiro@acm.org>
2024-01-05 23:06:05 -1000

    Run test-script on client sample scripts

--- client/test.scm
+++ client/test.scm
@@ -7,4 +7,21 @@
 (use chaton.client)
 (test-module 'chaton.client)
 
+(test-section "sample scripts")
+
+(test-script "examples/chaton-watcher")
+
+(cond-expand
+ [(library app.bsky)
+  (test-script "examples/chaton-bsky")]
+ [else])
+
+(cond-expand
+ [(and (library net.twitter.stream)
+       (library net.twitter.status)
+       (library net.twitter.friendship)
+       (library net.twitter.core))
+  (test-script "examples/chaton-twitter")]
+ [else])
+
 (test-end)
