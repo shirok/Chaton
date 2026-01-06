14eb43f6ea92f8b35b87ddd407471226ebd730bf
Shiro Kawai <shiro@acm.org>
2026-01-05 14:24:10 -1000

    Add package.scm just for metadata

---
+++ package.scm
@@ -0,0 +1,15 @@
+;;
+;; Package Chaton
+;;
+
+(define-gauche-package "Chaton"
+  :version "2.0"
+  :description "Comet-based chat server. "
+  :require (("Gauche" (>= "0.9.13")))
+  :providing-modules ()
+  :authors ("Shiro Kawai <shiro@acm.org>")
+  :maintainers ()
+  :licenses ("BSD")
+  :homepage "https://chaton.practical-scheme.net/"
+  :repository "https://github.com/shork/Chaton"
+  )
