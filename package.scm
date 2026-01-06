39833ac7ed232a658263c2a5e32e45cd9179da19
Shiro Kawai <shiro@acm.org>
2026-01-06 00:05:00 -1000

    Typo fix

--- package.scm
+++ package.scm
@@ -4,12 +4,12 @@
 
 (define-gauche-package "Chaton"
   :version "2.0"
-  :description "Comet-based chat server. "
+  :description "Comet-based chat server"
   :require (("Gauche" (>= "0.9.13")))
   :providing-modules ()
   :authors ("Shiro Kawai <shiro@acm.org>")
   :maintainers ()
   :licenses ("BSD")
   :homepage "https://chaton.practical-scheme.net/"
-  :repository "https://github.com/shork/Chaton"
+  :repository "https://github.com/shirok/Chaton"
   )
